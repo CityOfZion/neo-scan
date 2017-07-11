5fcaeff6bc83402e39bfe9ba04ad750f17eb2a64
Luciano Severino Engel <luciano_e@hotmail.com>
2017-07-10 22:59:57 -0300

    http module

---
+++ apps/neoscan/lib/neoscan/HttpCalls/http.ex
@@ -0,0 +1,53 @@
+defmodule Neoscan.HttpCalls do
+
+  @doc ~S"""
+   Returns seed url according with 'index'
+
+  ## Examples
+
+    iex> Neoscan.Blockchain.url(1)
+    "http://seed2.antshares.org:10332"
+
+  """
+  def url(index \\ 0) do
+    %{
+      0 => "https://localhost:20332",
+      1 => "http://seed2.antshares.org:10332",
+      2 => "http://seed3.antshares.org:10332",
+      3 => "http://seed4.antshares.org:10332",
+      4 => "http://seed5.antshares.org:10332",
+    }
+    # %{
+    #   0 => "http://seed1.antshares.org:10332",
+    #   1 => "http://seed2.antshares.org:10332",
+    #   2 => "http://seed3.antshares.org:10332",
+    #   3 => "http://seed4.antshares.org:10332",
+    #   4 => "http://seed5.antshares.org:10332",
+    # }
+    |> Map.get(index)
+  end
+
+  #Makes a request to the 'index' seed
+  def request(conn, headers, data, index) do
+    url(index)
+    |> HTTPoison.post( data, headers, ssl: [{:versions, [:'tlsv1.2']}] )
+    |> handle_response(conn)
+  end
+
+  #Handles the response of an HTTP call
+  defp handle_response(response, conn) do
+    case response do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        %{"result" => result} = Poison.decode!(body)
+        {conn, result}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "404 Not found :("
+        conn
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts "urlopen error, retry."
+        IO.inspect reason
+        conn
+    end
+  end
+
+end
