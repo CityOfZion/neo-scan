065f20ce733b61393aaa47baa60e937cad0ffd67
Luciano Severino Engel <luciano_e@hotmail.com>
2017-07-15 22:52:52 -0300

    debug

--- apps/neoscan/config/prod.exs
+++ apps/neoscan/config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
-
+  
 config :neoscan, Neoscan.Web.Endpoint,
-  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "radiant-eyrie-17530.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
