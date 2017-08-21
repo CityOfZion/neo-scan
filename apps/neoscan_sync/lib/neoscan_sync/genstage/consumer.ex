632090f3f094b5c99b0f3549f0d226284fcde6bf
lucianoengel <luciano_e@hotmail.com>
2017-08-20 21:55:07 -0300

    try

--- apps/neoscan_sync/lib/neoscan_sync/genstage/consumer.ex
+++ apps/neoscan_sync/lib/neoscan_sync/genstage/consumer.ex
@@ -8,7 +8,7 @@ defmodule NeoscanSync.Consumer do
   end
 
   def init(state) do
-    {:consumer, state, subscribe_to: [{NeoscanSync.Producer, max_demand: 300, min_demand: 100}]}
+    {:consumer, state, subscribe_to: [{NeoscanSync.Producer, max_demand: 100, min_demand: 50}]}
   end
 
   def handle_events(events, _from, state) do
