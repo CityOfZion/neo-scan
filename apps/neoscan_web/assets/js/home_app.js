17724a450f4a9d8ba641968fae753d72a1753fa7
lucianoengel <luciano_e@hotmail.com>
2017-08-22 20:33:15 -0300

    home reload

--- apps/neoscan_web/assets/js/home_app.js
+++ apps/neoscan_web/assets/js/home_app.js
@@ -2,24 +2,41 @@ import HomeSocket from "./home_socket"
 import Inferno from 'inferno';
 import List from 'inferno-virtual-list';
 
+var moment = require('moment');
+
 const blocksContainer = document.getElementById("show-blocks")
-// const transactionsContainer = document.getElementById('show-transactions')
+const transactionsContainer = document.getElementById('show-transactions')
 
-const Row = row => (
-  <a href="/block/{row.hash}" class="box">
-    <article class="media">
-      <div class="media-content">
-        <div class="content ">
+const blockRow = row => (
+  <a href={'/block/' + row.hash} class='box'>
+    <article class='media'>
+      <div class='media-content'>
+        <div class='content'>
           <p>
-            <strong>Height {row.index}</strong>
-            <small>Date
-            &nbsp&nbsp&nbsp
+            <strong>Height {row.index}&nbsp;&nbsp;&nbsp;</strong>
+            <small>Date&nbsp;&nbsp;{moment.unix(row.time).format('DD-MM-YYYY')}&nbsp;&nbsp;
             </small>
-            <small>Time
+            <small>Time&nbsp;&nbsp;{moment.unix(row.time).format('HH:mm:ss')}&nbsp;&nbsp;
+            </small>
+            <small class='is-pulled-right'>Number of Transactions {row.tx_count}
+            </small>
+          </p>
+        </div>
+      </div>
+    </article>
+  </a>
+)
 
-            &nbsp&nbsp&nbsp
+const transactionRow = row => (
+  <a href={'/transaction/' + row.txid} class='box'>
+    <article class='media'>
+      <div class='media-content'>
+        <div class='content'>
+          <p>
+            <strong>{row.type}&nbsp;&nbsp;&nbsp;</strong>
+            <small>Date&nbsp;&nbsp;{moment.unix(row.time).format('DD-MM-YYYY')}&nbsp;&nbsp;
             </small>
-            <small class="is-pulled-right">Number of Transactions {row.tx_count}
+            <small>Time&nbsp;&nbsp;{moment.unix(row.time).format('HH:mm:ss')}&nbsp;&nbsp;
             </small>
           </p>
         </div>
@@ -35,11 +52,15 @@ window.onload = function () {
   }
   let home = new HomeSocket(payload)
 
-  setInterval(function(){
+  setInterval(function () {
     Inferno.render((
-      <List class="collection" sync={false} data={home.payload.blocks} rowHeight={15} rowRender={Row} />
+      <List class='collection' sync={false} data={home.payload.blocks} rowHeight={15} rowRender={blockRow} />
     ), blocksContainer)
-  }, 500);
+
+    Inferno.render((
+      <List class='collection' sync={false} data={home.payload.transactions} rowHeight={15} rowRender={transactionRow} />
+    ), transactionsContainer)
+  }, 500)
 
   home.connect()
 }
