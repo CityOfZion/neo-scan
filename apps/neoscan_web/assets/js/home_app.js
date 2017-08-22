b58288e29a558ffa32671083ecc584ddd51faee8
Luciano Severino Engel <luciano_e@hotmail.com>
2017-08-22 18:02:17 -0300

    front-end

--- apps/neoscan_web/assets/js/home_app.js
+++ apps/neoscan_web/assets/js/home_app.js
@@ -1,17 +1,32 @@
 import HomeSocket from "./home_socket"
-import { h, render, Component } from 'preact'
+import Inferno from 'inferno';
+import List from 'inferno-virtual-list';
 
 const blocksContainer = document.getElementById("show-blocks")
 // const transactionsContainer = document.getElementById('show-transactions')
 
-class blockList extends Component {
-  render (props, state) {
-    // props === this.props
-    // state === this.state
-    return "<h1>Hello, {props}!</h1>"
-  }
-}
-// data binding
+const Row = row => (
+  <a href="/block/{row.hash}" class="box">
+    <article class="media">
+      <div class="media-content">
+        <div class="content ">
+          <p>
+            <strong>Height {row.index}</strong>
+            <small>Date
+            &nbsp&nbsp&nbsp
+            </small>
+            <small>Time
+
+            &nbsp&nbsp&nbsp
+            </small>
+            <small class="is-pulled-right">Number of Transactions {row.tx_count}
+            </small>
+          </p>
+        </div>
+      </div>
+    </article>
+  </a>
+)
 
 window.onload = function () {
   let payload = {
@@ -19,9 +34,12 @@ window.onload = function () {
     transactions: []
   }
   let home = new HomeSocket(payload)
-  blockList.props = home.payload.blocks
-  render(blockList, blocksContainer) // call data binding
-  // let app2 = view_transactions(home) // call data binding
+
+  setInterval(function(){
+    Inferno.render((
+      <List class="collection" sync={false} data={home.payload.blocks} rowHeight={15} rowRender={Row} />
+    ), blocksContainer)
+  }, 500);
 
   home.connect()
 }
