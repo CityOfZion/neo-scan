a94c34d935e4001f45d35b296f10c3b5f0d7999a
lucianoengel <luciano_e@hotmail.com>
2017-08-24 23:50:47 -0300

    socket origin

--- apps/neoscan_web/config/prod.exs
+++ apps/neoscan_web/config/prod.exs
@@ -18,7 +18,8 @@ http: [port: {:system, "PORT"}, compress: true],
 url: [scheme: "https", host: "neoscan.io", port: 443],
 force_ssl: [rewrite_on: [:x_forwarded_proto]],
 cache_static_manifest: "priv/static/cache_manifest.json",
-secret_key_base: System.get_env("SECRET_KEY_BASE")
+secret_key_base: System.get_env("SECRET_KEY_BASE"),
+check_origin: ["neoscan.io", "www.neoscan.io", "neoscan.herokuapp.com"]
 
 # ## SSL Support
 #
