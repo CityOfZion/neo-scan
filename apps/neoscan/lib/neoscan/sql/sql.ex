a8106bea8e4c2129d5aa77f8f8318226899bf35d
LucianoEngel <luciano_e@hotmail.com>
2017-09-04 12:31:13 -0300

    pack in multi

--- apps/neoscan/lib/neoscan/sql/sql.ex
+++ apps/neoscan/lib/neoscan/sql/sql.ex
@@ -1,13 +1,13 @@
 defmodule Neoscan.Sql do
 
-  def add_tx(hash, tx) do
+  def add_tx(_changes, hash, tx_ids) do
     sql = "UPDATE addresses SET tx_ids = (
           CASE
               WHEN tx_ids IS NULL THEN '{}'::JSONB
               ELSE tx_ids
           END
         ) || $2 ::JSONB WHERE address = $1;"
-    Ecto.Adapters.SQL.query!(Neoscan.Repo, sql, [hash, tx])
+    Ecto.Adapters.SQL.query(Neoscan.Repo, sql, [hash, tx_ids])
   end
 
 end
