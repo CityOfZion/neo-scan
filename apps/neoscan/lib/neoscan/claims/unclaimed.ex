c851a4bb9bce114cc048ad4f114d579619ac6797
Luciano Severino Engel <luciano_e@hotmail.com>
2017-09-27 15:15:22 -0300

    unclaimed calculation done

--- apps/neoscan/lib/neoscan/claims/unclaimed.ex
+++ apps/neoscan/lib/neoscan/claims/unclaimed.ex
@@ -5,6 +5,33 @@ defmodule Neoscan.Claims.Unclaimed do
   alias NeoscanMonitor.Api
   alias Neoscan.Blocks.Block
 
+  #total amount of available NEO
+  def total_neo, do: 100000000
+
+  #calculate unclaimed gas bonus
+  def calculate_bonus(address_id) do
+    get_unclaimed_vouts(address_id)
+    |> add_end_height
+    |> route_if_there_is_unclaimed
+  end
+
+  def route_if_there_is_unclaimed([]) do
+    0
+  end
+  def route_if_there_is_unclaimed(unclaimed) do
+    blocks_with_gas = get_unclaimed_block_range(unclaimed)
+                       |> get_blocks_gas
+
+    Enum.reduce(unclaimed,0, fn (vout, acc) -> acc + compute_vout_bonus(vout, blocks_with_gas) end)
+  end
+
+  #compute bonus for a singel vout
+  def compute_vout_bonus(%{:value => value, :start_height => start_height, :end_height => end_height}, blocks_with_gas) do
+    total_gas = Enum.filter(blocks_with_gas, fn %{:index => index} -> index >= start_height && index <= end_height end)
+                |> Enum.reduce(0, fn(%{:gas => gas}, acc) -> acc + gas end)
+
+    total_gas * value / total_neo()
+  end
 
 
   #get all unclaimed transaction vouts
@@ -16,13 +43,16 @@ defmodule Neoscan.Claims.Unclaimed do
     Repo.all(query)
   end
 
+  def add_end_height(unclaimed_vouts) do
+    Enum.map(unclaimed_vouts, fn %{:end_height => height} = vout -> Map.put(vout, :end_height, check_end_height(height) ) end)
+  end
+
   #get block range of unclaimed transaction vouts
   def get_unclaimed_block_range(unclaimed_vouts) do
-    end_height_list = Enum.map(unclaimed_vouts, fn %{:end_height => end_height} -> end_height end)
+    end_height_list = Enum.map(unclaimed_vouts, fn %{:end_height => end_height} -> check_end_height(end_height) end)
 
     max = end_height_list
-          |> Enum.member?(nil)
-          |> check_end_height(end_height_list)
+          |> Enum.max
 
     min = Enum.map(unclaimed_vouts, fn %{:start_height => start_height} -> start_height end)
           |> Enum.min
@@ -31,21 +61,22 @@ defmodule Neoscan.Claims.Unclaimed do
   end
 
   #check end_height and use current height when transaction vout wasn't used
-  def check_end_height(true, _list) do
-    Api.get_height
+  def check_end_height(nil) do
+    {:ok, height} = Api.get_height
+    height
   end
-  def check_end_height(false, list) do
-    Enum.max(list)
+  def check_end_height(number) do
+    number
   end
 
-  #get total gas distribution amount for all blocks in a given range tuple 
+  #get total gas distribution amount for all blocks in a given range tuple
   def get_blocks_gas({min,max}) do
     query = from b in Block,
      where: b.index >= ^min and b.index <= ^max,
-     select: map(b, [:index, :total_sys_fee, :total_net_fee, :gas_generated])
+     select: map(b, [:index, :total_sys_fee, :gas_generated])
 
     Repo.all(query)
-    |> Enum.map(fn %{:index => index, :total_sys_fee => sys, :total_net_fee => net, :gas_generated => gen} -> %{:index => index, :gas => sys+net+gen} end)
+    |> Enum.map(fn %{:index => index, :total_sys_fee => sys, :gas_generated => gen} -> %{:index => index, :gas => sys+gen} end)
   end
 
 
