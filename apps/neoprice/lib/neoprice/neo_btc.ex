a99302c4258c5613ecb7c515779c526ff87e1384
Steffel <steffel.fenix@gmail.com>
2017-10-13 20:14:57 +0800

    extract cache config

--- apps/neoprice/lib/neoprice/neo_btc.ex
+++ apps/neoprice/lib/neoprice/neo_btc.ex
@@ -1,5 +1,48 @@
 defmodule Neoprice.NeoBtc do
   @moduledoc false
-  use Neoprice.Cache, from_symbol: "NEO", to_symbol: "BTC"
 
+  @day 86400
+  @month 2678400
+
+  alias Neoprice.Cache.Config
+  use Neoprice.Cache,
+      from_symbol: "NEO",
+      to_symbol: "BTC",
+      config: [
+        %Config{
+          cache_name: :NEOBTC_day,
+          definition: :day,
+          duration: :start,
+          aggregation: 1
+        },
+        %Config{
+          cache_name: :NEOBTC_3_m,
+          definition: :hour,
+          duration: @month * 3,
+          aggregation: 3
+        },
+        %Config{
+          cache_name: :NEOBTC_1_m,
+          definition: :hour,
+          duration: @month,
+          aggregation: 1
+        },
+        %Config{
+          cache_name: :NEOBTC_1_w,
+          definition: :minute,
+          duration: 7 * @day,
+          aggregation: 15
+        },
+        %Config{
+          cache_name: :NEOBTC_1_d,
+          definition: :minute,
+          duration: @day,
+          aggregation: 1
+        }
+      ]
+  def get_day(), do: :ets.tab2list(:NEOBTC_day) |> Map.new
+  def get_3_month(), do: :ets.tab2list(:NEOBTC_3_m) |> Map.new
+  def get_1_month(), do: :ets.tab2list(:NEOBTC_1_m) |> Map.new
+  def get_1_week(), do: :ets.tab2list(:NEOBTC_1_w) |> Map.new
+  def get_1_day(), do: :ets.tab2list(:NEOBTC_1_d) |> Map.new
 end
