2ad5e2fa40042472fee498d85bcf3d90a68121de
LucianoEngel <luciano_e@hotmail.com>
2017-11-06 11:17:49 -0200

    genstage strategy

--- apps/neoscan_sync/lib/neoscan_sync/application.ex
+++ apps/neoscan_sync/lib/neoscan_sync/application.ex
@@ -21,7 +21,7 @@ defmodule NeoscanSync.Application do
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: NeoscanSync.Supervisor]
+    opts = [strategy: :one_for_all, name: NeoscanSync.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
