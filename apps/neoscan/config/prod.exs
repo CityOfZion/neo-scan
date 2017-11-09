e8c2e8f48b9a0f5d6af54f3e372ab4beb31aeb00
LucianoEngel <luciano_e@hotmail.com>
2017-11-09 09:05:40 -0200

    db timout

--- apps/neoscan/config/prod.exs
+++ apps/neoscan/config/prod.exs
@@ -6,6 +6,7 @@ config :neoscan, Neoscan.Repo,
   password: "postgres",
   database: "neoscan_prodv",
   hostname: "localhost",
-  pool_size: 30
+  pool_size: 30,
+  pool_timout: 15_000
 
 #import_config "prod.secret.exs"
