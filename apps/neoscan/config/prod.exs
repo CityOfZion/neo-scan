e30bad7203bd0f59b7c49fe1218c862bc573d50a
LucianoEngel <luciano_e@hotmail.com>
2017-11-09 09:13:32 -0200

    typo

--- apps/neoscan/config/prod.exs
+++ apps/neoscan/config/prod.exs
@@ -7,6 +7,6 @@ config :neoscan, Neoscan.Repo,
   database: "neoscan_prodv",
   hostname: "localhost",
   pool_size: 30,
-  pool_timout: 15_000
+  pool_timeout: 15_000
 
 #import_config "prod.secret.exs"
