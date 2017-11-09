72dae39421d4f7194551b6e516da442124b79a0f
LucianoEngel <luciano_e@hotmail.com>
2017-11-09 11:11:28 -0200

    increase demand

--- apps/neoscan_sync/lib/neoscan_sync/genstage/consumer.ex
+++ apps/neoscan_sync/lib/neoscan_sync/genstage/consumer.ex
@@ -14,7 +14,7 @@ defmodule NeoscanSync.Consumer do
     {
       :consumer,
       state,
-      subscribe_to: [{NeoscanSync.Producer, max_demand: 50, min_demand: 25}]
+      subscribe_to: [{NeoscanSync.Producer, max_demand: 500, min_demand: 250}]
     }
   end
 
