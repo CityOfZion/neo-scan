ee6c905bba316bbd493bfd669a96cefe8a0cc4bc
LucianoEngel <luciano_e@hotmail.com>
2017-11-12 12:37:25 -0200

    fix index

--- apps/neoscan/priv/repo/migrations/20170905193507_histories.exs
+++ apps/neoscan/priv/repo/migrations/20170905193507_histories.exs
@@ -17,6 +17,7 @@ defmodule Neoscan.Repo.Migrations.Histories do
     create index(:histories, [:address_hash])
     create index(:histories, [:address_id])
     create index(:histories, [:time])
+    create index(:histories, ["block_height DESC NULLS LAST"])
 
   end
 end
