26370d4f7a41f90a7c3f48b5e09a78db2ea87677
Silas Albrecht <silas.a.a@gmail.com>
2018-02-06 19:25:47 -0500

    Separate out seed configuration

--- apps/neoscan_monitor/config/config.exs
+++ apps/neoscan_monitor/config/config.exs
@@ -1,3 +1,17 @@
 use Mix.Config
 
 config :neoscan_monitor, ecto_repos: []
+
+config :neoscan_monitor, seeds: [
+      "http://seed1.cityofzion.io:8080",
+      "http://seed2.cityofzion.io:8080",
+      "http://seed3.cityofzion.io:8080",
+      "http://seed4.cityofzion.io:8080",
+      "http://seed5.cityofzion.io:8080",
+      "http://api.otcgo.cn:10332",
+      "http://seed1.neo.org:10332",
+      "http://seed2.neo.org:10332",
+      "http://seed3.neo.org:10332",
+      "http://seed4.neo.org:10332",
+      "http://seed5.neo.org:10332"
+    ]
