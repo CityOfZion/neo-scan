981bbdcd140a416db5037cbb4413d83925d05f1a
LucianoEngel <luciano_e@hotmail.com>
2018-03-26 11:00:49 -0300

    fix jp language

--- apps/neoscan_web/lib/neoscan_web/plugs/locale.ex
+++ apps/neoscan_web/lib/neoscan_web/plugs/locale.ex
@@ -1,7 +1,7 @@
 defmodule NeoscanWeb.Plugs.Locale do
   @moduledoc false
   alias Plug.Conn
-  @languages ["en", "nl", "fr", "pt-br", "it", "de", "ru", "ro", "jp", "zh"]
+  @languages ["en", "nl", "fr", "pt-br", "it", "de", "ru", "ro", "ja", "zh"]
 
   def init(opts) do
     opts
