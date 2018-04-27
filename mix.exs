d505621c993a5e86cb5afae68cb74f7d6230fd4d
Adrien <adrien.moreau@backslash.fr>
2018-04-27 16:47:05 +0700

    add coveralls in dependencies

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule Neoscan.Umbrella.Mixfile do
       {:httpoison, "~> 0.13"},
       {:flow, "~> 0.11"},
       {:ex_machina, "~> 2.0", only: [:test, :travis]},
+      {:excoveralls, "~> 0.8", only: [:test, :travis]},
       {:morphix, "~> 0.0.7"}
     ]
   end
