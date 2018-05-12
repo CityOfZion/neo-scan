494e051bc3d82038708a206d082cbbdefd40e96e
Adrien <adrien.moreau@backslash.fr>
2018-05-12 12:34:10 +0700

    add haproxy

--- apps/neoscan_web/config/prod.exs
+++ apps/neoscan_web/config/prod.exs
@@ -15,12 +15,12 @@ use Mix.Config
 # which you typically run after static files are built.
 config :neoscan_web, NeoscanWeb.Endpoint,
   http: [port: {:system, "PORT"}, compress: true],
-  url: [scheme: "https", host: "neoscan.io", port: 443],
+  url: [scheme: "https", host: {:system, "HOST"}, port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "set_key",
-  check_origin: ["https://neoscan.io", "https://www.neoscan.io"]
+  check_origin: ["https://neoscan.io", "https://www.neoscan.io", "https://neoscan.backslash.fr"]
 
 # ## SSL Support
 #
