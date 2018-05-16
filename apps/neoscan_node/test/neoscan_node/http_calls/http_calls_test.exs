f0d332d03b09a9bd21223d1645fe54e3ca865edc
Adrien <adrien.moreau@backslash.fr>
2018-05-16 15:48:42 +0700

    add http calls tests

---
+++ apps/neoscan_node/test/neoscan_node/http_calls/http_calls_test.exs
@@ -0,0 +1,27 @@
+defmodule Neoscan.HttpCalls.HttpCallsTest do
+  use ExUnit.Case
+
+  alias NeoscanNode.HttpCalls
+  import ExUnit.CaptureLog
+
+  @node_url "http://seed1.cityofzion.io:8080"
+
+  test "get_url/1" do
+    assert 2 == Enum.count(HttpCalls.get_url(2))
+    assert 1 == Enum.count(HttpCalls.get_url(1))
+    assert 10_000 > Enum.count(HttpCalls.get_url(10_000))
+  end
+
+  test "request/3" do
+    data = "{\"params\":[0,1],\"method\":\"getblock\",\"jsonrpc\":\"2.0\",\"id\":5}"
+    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
+    assert {:ok, _} = HttpCalls.request(headers, data, @node_url)
+    assert {:ok, _} = HttpCalls.request(headers, data, [@node_url])
+
+    assert capture_log(fn -> assert :ok == HttpCalls.request(headers, data, 123) end) =~
+             "Error in url"
+
+    data = "{\"params\":[0,1],\"method\":\"getblockerror\",\"jsonrpc\":\"2.0\",\"id\":5}"
+    assert {:error, _} = HttpCalls.request(headers, data, @node_url)
+  end
+end
