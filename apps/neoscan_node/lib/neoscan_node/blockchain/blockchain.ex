be3091e6fc4ba6260a3dd19cfa31f8f7420af577
Adrien <adrien.moreau@backslash.fr>
2018-05-16 14:36:05 +0700

    refactor blockchain module

--- apps/neoscan_node/lib/neoscan_node/blockchain/blockchain.ex
+++ apps/neoscan_node/lib/neoscan_node/blockchain/blockchain.ex
@@ -7,84 +7,26 @@ defmodule NeoscanNode.Blockchain do
 
   alias NeoscanNode.HttpCalls
 
+  defp request(url, method, params) do
+    data =
+      Poison.encode!(%{"jsonrpc" => "2.0", "method" => method, "params" => params, "id" => 5})
+
+    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
+    HttpCalls.request(headers, data, url)
+  end
+
   @doc """
    Get the current block by height through seed 'index'
   """
-  def get_block_by_height(url, height) do
-    data =
-      Poison.encode!(%{
-        "jsonrpc" => "2.0",
-        "method" => "getblock",
-        "params" => [height, 1],
-        "id" => 5
-      })
+  def get_block_by_height(url, height), do: request(url, "getblock", [height, 1])
 
-    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
-    HttpCalls.request(headers, data, url)
-  end
+  def get_block_by_hash(url, hash), do: request(url, "getblock", [hash, 1])
 
-  def get_block_by_hash(url, hash) do
-    data =
-      Poison.encode!(%{
-        "jsonrpc" => "2.0",
-        "method" => "getblock",
-        "params" => [hash, 1],
-        "id" => 5
-      })
+  def get_current_height(url), do: request(url, "getblockcount", [])
 
-    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
-    HttpCalls.request(headers, data, url)
-  end
+  def get_transaction(url, txid), do: request(url, "getrawtransaction", [txid, 1])
 
-  def get_current_height(url) do
-    data =
-      Poison.encode!(%{
-        jsonrpc: "2.0",
-        method: "getblockcount",
-        params: [],
-        id: 5
-      })
+  def get_asset(url, txid), do: request(url, "getassetstate", [txid, 1])
 
-    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
-    HttpCalls.request(headers, data, url)
-  end
-
-  def get_transaction(url, txid) do
-    data =
-      Poison.encode!(%{
-        jsonrpc: "2.0",
-        method: "getrawtransaction",
-        params: [txid, 1],
-        id: 5
-      })
-
-    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
-    HttpCalls.request(headers, data, url)
-  end
-
-  def get_asset(url, txid) do
-    data =
-      Poison.encode!(%{
-        jsonrpc: "2.0",
-        method: "getassetstate",
-        params: [txid, 1],
-        id: 5
-      })
-
-    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
-    HttpCalls.request(headers, data, url)
-  end
-
-  def get_contract(url, hash) do
-    data =
-      Poison.encode!(%{
-        jsonrpc: "2.0",
-        method: "getcontractstate",
-        params: [hash],
-        id: 5
-      })
-
-    headers = [{"Content-Type", "application/json"}, {"Accept-Encoding", "gzip"}]
-    HttpCalls.request(headers, data, url)
-  end
+  def get_contract(url, hash), do: request(url, "getcontractstate", [hash])
 end
