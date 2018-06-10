d023989ec2ceed4a3e03bdd07b6b7f4253c9ec66
Adrien <adrien.moreau@backslash.fr>
2018-06-10 22:53:36 +0800

    add trigger for balances

--- apps/neoscan/priv/repo/migrations/20180607000007_address_balances.exs
+++ apps/neoscan/priv/repo/migrations/20180607000007_address_balances.exs
@@ -41,9 +41,31 @@ defmodule Neoscan.Repo.Migrations.AddressBalances do
     """
 
     execute """
-      CREATE TRIGGER generate_address_balnce_from_address_history_trigger
+      CREATE TRIGGER generate_address_balance_from_address_history_trigger
       AFTER INSERT ON address_histories FOR each row
       EXECUTE PROCEDURE generate_address_balances_from_address_history();
+    """
+
+    execute """
+    CREATE OR REPLACE FUNCTION generate_address_from_address_history() RETURNS TRIGGER LANGUAGE plpgsql AS $body$
+      BEGIN
+        INSERT INTO addresses (hash, first_transaction_time, last_transaction_time, tx_count, inserted_at, updated_at)
+        VALUES (NEW.address_hash, NEW.block_time, NEW.block_time, 1, NEW.inserted_at, NEW.updated_at)
+        ON CONFLICT ON CONSTRAINT addresses_pkey DO
+        UPDATE SET
+        tx_count = addresses.tx_count + EXCLUDED.tx_count,
+        first_transaction_time = LEAST(addresses.first_transaction_time, EXCLUDED.first_transaction_time),
+        last_transaction_time = GREATEST(addresses.last_transaction_time, EXCLUDED.last_transaction_time),
+        updated_at = EXCLUDED.updated_at;
+        RETURN NULL;
+      END;
+      $body$;
+    """
+
+    execute """
+      CREATE TRIGGER generate_address_from_address_history_trigger
+      AFTER INSERT ON address_histories FOR each row
+      EXECUTE PROCEDURE generate_address_from_address_history();
     """
   end
 end
