9142d63a3800007a742798a6973446873d69d668
Adrien <adrien.moreau@backslash.fr>
2018-06-10 21:45:57 +0800

    add trigger for address histories

--- apps/neoscan/priv/repo/migrations/20180607000006_address_histories.exs
+++ apps/neoscan/priv/repo/migrations/20180607000006_address_histories.exs
@@ -3,7 +3,7 @@ defmodule Neoscan.Repo.Migrations.AddressHistories do
 
   def change do
     create table(:address_histories, primary_key: false) do
-      add(:hash, :binary, primary_key: true)
+      add(:address_hash, :binary, primary_key: true)
       add(:asset, :binary, null: false)
       add(:value, :float, null: false)
       add(:block_time,  :naive_datetime, null: false)
--- apps/neoscan/priv/repo/migrations/20180607000007_address_balances.exs
+++ apps/neoscan/priv/repo/migrations/20180607000007_address_balances.exs
@@ -3,11 +3,28 @@ defmodule Neoscan.Repo.Migrations.AddressBalances do
 
   def change do
     create table(:address_balances, primary_key: false) do
-      add(:hash, :binary, primary_key: true)
+      add(:address_hash, :binary, primary_key: true)
       add(:asset, :binary, primary_key: true)
       add(:value, :float, null: false)
 
       timestamps()
     end
+
+    execute """
+    CREATE OR REPLACE FUNCTION generate_address_history_from_vouts() RETURNS TRIGGER LANGUAGE plpgsql AS $body$
+      BEGIN
+        INSERT INTO address_histories (address_hash, asset, value, block_time, inserted_at, updated_at)
+        VALUES (NEW.address_hash, NEW.asset, NEW.value, NEW.block_time, NEW.inserted_at, NEW.updated_at);
+        RETURN NULL;
+      END;
+      $body$;
+
+    """
+
+    execute """
+      CREATE TRIGGER generate_address_history_from_vouts_trigger
+      AFTER INSERT ON vouts FOR each row
+      EXECUTE PROCEDURE generate_address_history_from_vouts();
+    """
   end
 end
