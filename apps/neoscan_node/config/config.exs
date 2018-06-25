0cf4d0647495f32fa53539a125143729c63c316b
Adrien <adrien.moreau@backslash.fr>
2018-06-25 14:49:01 +0200

    fix notifications

--- apps/neoscan_node/config/config.exs
+++ apps/neoscan_node/config/config.exs
@@ -10,7 +10,8 @@ else
     notification_seeds: [
       "http://notifications1.neeeo.org/v1",
       "http://notifications2.neeeo.org/v1",
-      "http://notifications3.neeeo.org/v1"
+      "http://notifications3.neeeo.org/v1",
+      "http://notifications4.neeeo.org/v1"
     ]
 end
 
