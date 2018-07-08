6b0e988885faee43932c005917bfd4a04c1d7222
Adrien <adrien.moreau@backslash.fr>
2018-07-08 18:42:11 +0200

    forgot wrapper

--- apps/neoscan_cache/lib/neoscan_cache/crypto_compare_wrapper.ex
+++ apps/neoscan_cache/lib/neoscan_cache/crypto_compare_wrapper.ex
@@ -2,4 +2,8 @@ defmodule NeoscanCache.CryptoCompareWrapper do
   def pricemultifull(list1, list2) do
     CryptoCompare.pricemultifull(list1, list2)
   end
+
+  def histo_day(from, to, opts), do: CryptoCompare.histo_day(from, to, opts)
+  def histo_hour(from, to, opts), do: CryptoCompare.histo_hour(from, to, opts)
+  def histo_minute(from, to, opts), do: CryptoCompare.histo_minute(from, to, opts)
 end
