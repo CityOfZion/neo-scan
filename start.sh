ea0cae43dd209e10bb96d9ea195247db205f4507
Adrien <adrien.moreau@backslash.fr>
2018-07-14 01:17:05 +0200

    try script

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+DOC=`ls /opt/app/lib/neoscan_web-*/priv/static/docs/index.html`
+sed -i.bak 's/main_net/test_net/g' $DOC
+sed -i.bak "s/neoscan.io/$HOST/g" $DOC
+mv $DOC /tmp/file
+rm /opt/app/lib/neoscan_web-*/priv/static/docs/*
+mv /tmp/file $DOC
 
 /opt/app/bin/neoscan migrate
 /opt/app/bin/neoscan foreground
