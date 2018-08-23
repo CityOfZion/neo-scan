6c2f8981f5c8f1f7e06b6466bb0d1c23d775cd20
Adrien <adrien.moreau@backslash.fr>
2018-08-23 12:10:34 +0200

    add infinity timeout/do not crash

--- apps/neoscan/lib/neoscan/flush.ex
+++ apps/neoscan/lib/neoscan/flush.ex
@@ -2,19 +2,21 @@ defmodule Neoscan.Flush do
   alias Neoscan.Repo
 
   def address_balances do
-    Ecto.Adapters.SQL.query!(Repo, "SELECT flush_address_balances_queue()", [])
+    Ecto.Adapters.SQL.query(Repo, "SELECT flush_address_balances_queue()", [], timeout: :infinity)
   end
 
   def addresses do
-    Ecto.Adapters.SQL.query!(Repo, "SELECT flush_addresses_queue()", [])
+    Ecto.Adapters.SQL.query(Repo, "SELECT flush_addresses_queue()", [], timeout: :infinity)
   end
 
   def address_transaction_balances do
-    Ecto.Adapters.SQL.query!(Repo, "SELECT flush_address_transaction_balances_queue()", [])
+    Ecto.Adapters.SQL.query(Repo, "SELECT flush_address_transaction_balances_queue()", [],
+      timeout: :infinity
+    )
   end
 
   def vouts do
-    Ecto.Adapters.SQL.query!(Repo, "SELECT flush_vouts_queue()", [])
+    Ecto.Adapters.SQL.query(Repo, "SELECT flush_vouts_queue()", [], timeout: :infinity)
   end
 
   def all do
