56bb5fed7f29d8683aa19a633116c11772eae75f
Adrien <adrien.moreau@backslash.fr>
2018-09-02 16:44:48 +0200

    remove python endpoints

--- apps/neoscan_node/config/config.exs
+++ apps/neoscan_node/config/config.exs
@@ -17,14 +17,7 @@ end
 
 config :neoscan_node,
   seeds: [
-    "http://node2.nyc3.bridgeprotocol.io:10332",
     "http://seed4.aphelion-neo.com:10332",
-    "http://node2.sgp1.bridgeprotocol.io:10332",
-    "http://node2.ams2.bridgeprotocol.io:10332",
-    "https://pyrpc1.narrative.org:443",
-    "https://pyrpc4.narrative.org:443",
-    "https://pyrpc2.narrative.org:443",
-    "https://pyrpc3.narrative.org:443",
     "http://seed3.aphelion-neo.com:10332",
     "http://seed2.aphelion-neo.com:10332",
     "http://seed5.ngd.network:10332",
@@ -45,18 +38,13 @@ config :neoscan_node,
     "http://seed2.travala.com:10332",
     "https://seed1.switcheo.network:10331",
     "http://seed7.ngd.network:10332",
-    "http://node1.ams2.bridgeprotocol.io:10332",
     "https://seed8.cityofzion.io:443",
     "http://rustylogic.ddns.net:10332",
     "http://api.otcgo.cn:10332",
     "https://seed6.cityofzion.io:443",
     "https://seed7.cityofzion.io:443",
     "https://seed0.cityofzion.io:443",
-    "https://seed9.cityofzion.io:443",
-    "http://pyrpc2.redpulse.com:10332",
-    "http://pyrpc1.redpulse.com:10332",
-    "https://pyrpc1.redpulse.com:10331",
-    "https://pyrpc2.redpulse.com:10331"
+    "https://seed9.cityofzion.io:443"
   ]
 
 if Mix.env() == :test do
