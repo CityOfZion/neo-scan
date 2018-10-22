94db20426ae27cd3da022b5ed562aac241afa97b
steffel fenix <steffel.fenix@gmail.com>
2018-10-22 21:18:55 +0800

    add schema for single block query

--- apps/neoscan_web/test/neoscan_web/schema/query/block_test.exs
+++ apps/neoscan_web/test/neoscan_web/schema/query/block_test.exs
@@ -66,4 +66,106 @@ defmodule NeoscanWeb.Schema.Query.BlockTest do
 
     assert length(rows) == 2
   end
+
+  @query """
+  query Block($params: Params!){
+  block(params: $params){
+      cumulative_sys_fee
+      gasGenerated
+      hash
+      index
+      inserted_at
+      lag
+      merkle_root
+      next_consensus
+      nonce
+      script {
+        invocation
+        verification
+      }
+  }
+  }
+  """
+
+  test "get one block by index", %{conn: conn} do
+    block = insert(:block)
+    index = block.index
+
+    conn =
+      post(
+        conn,
+        "/graphql",
+        %{
+          query: @query,
+          variables: %{
+            params: %{
+              index: index
+            }
+          }
+        }
+      )
+
+    body = json_response(conn, 200)
+
+    assert %{
+             "data" => %{
+               "block" => %{
+                 "cumulative_sys_fee" => _,
+                 "gasGenerated" => _,
+                 "hash" => _,
+                 "index" => ^index,
+                 "inserted_at" => _,
+                 "lag" => _,
+                 "merkle_root" => _,
+                 "next_consensus" => _,
+                 "nonce" => _,
+                 "script" => %{
+                   "invocation" => _,
+                   "verification" => _
+                 }
+               }
+             }
+           } = body
+  end
+
+  test "get one block by hash", %{conn: conn} do
+    block = insert(:block)
+    hash = block.hash
+
+    conn =
+      post(
+        conn,
+        "/graphql",
+        %{
+          query: @query,
+          variables: %{
+            params: %{
+              hash: block.hash
+            }
+          }
+        }
+      )
+
+    body = json_response(conn, 200)
+
+    assert %{
+             "data" => %{
+               "block" => %{
+                 "cumulative_sys_fee" => _,
+                 "gasGenerated" => _,
+                 "hash" => ^hash,
+                 "index" => _,
+                 "inserted_at" => _,
+                 "lag" => _,
+                 "merkle_root" => _,
+                 "next_consensus" => _,
+                 "nonce" => _,
+                 "script" => %{
+                   "invocation" => _,
+                   "verification" => _
+                 }
+               }
+             }
+           } = body
+  end
 end
